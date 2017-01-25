d0da97d39a6076452e476de128c9e535acc31bc5
ladydangerdame <turbofemme@gmail.com>
2017-01-24 22:00:31 -0800

    Added username in place of email and fixed bugs in comments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,18 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+    before_action :configure_permitted_parameters, if: :devise_controller?
+    protect_from_forgery with: :exception
 
-  before_filter :initialize_message
+    before_filter :initialize_message
 
- def initialize_message
-   @message = Message.new
- end
- 
+    protected
+
+    def configure_permitted_parameters
+        added_attrs = [:username, :email, :password, :password_confirmation, :remember_me]
+        devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+        devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+  end
+
+    def initialize_message
+        @message = Message.new
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,17 @@
 class User < ApplicationRecord
   has_many :messages
   has_many :comments
+
+  def login=(login)
+    @login = login
+  end
+
+  def login
+    @login || self.username || self.email
+  end
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :authentication_keys => [:login]
 end
